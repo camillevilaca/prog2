ea6366b4e8516b3f05f0f389ecd3132a5dcc5cb6
IGOR <lazy2martines@hotmail.com>
2019-03-20 10:30:11 -0300

    enviar- 20/03/19

--- servidor.py
+++ servidor.py
@@ -1,4 +1,5 @@
 from flask import Flask , render_template
+from modelo import Pessoa
 app = Flask (__name__)
 
 @app.route("/form_atualizar_pessoa")
@@ -16,10 +17,16 @@ def exibir():
 
 @app.route("/listar-pessoa")
 def listar ():
-    return render_template("listar-pessoa.html")
+    lista = [Pessoa("Manoel Carlos Farias","manoelcarlosfarias_@sanidet.com.br"),
+    Pessoa("Luís Ferreira","luisrodriguesferreira@rhyta.com"),
+    Pessoa("Stephanie Lowman","stephanieclowman@teleworm.us")
+    ]  
+    return render_template("listar-pessoa.html", geral=lista)
 
 @app.route("/")
 def listar_padrao():
     return render_template("listar-pessoa.html")
 
 app.run()
+
+
